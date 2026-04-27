f60fc25e877be04ea00065d90a715d17b4b3da72
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-04-27 10:20:05 +0100

    test pushing

--- src/main/java/com/example/HealthCare/model/dto/PatientDTO.java
+++ src/main/java/com/example/HealthCare/model/dto/PatientDTO.java
@@ -1,4 +1,15 @@
 package com.example.HealthCare.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
 public class PatientDTO {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private LocalDate dateNaissance;
 }
